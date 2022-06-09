d9842305c6851f4015540c686d45fee5d76048dd
Nwadike Philip <philiponwadike123@gmail.com>
2022-06-09 17:20:40 +0100

    Initial Commit

---
+++ app/src/main/java/com/epicdevler/practice/mediaplayerimpl/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.epicdevler.practice.mediaplayerimpl
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.epicdevler.practice.mediaplayerimpl.ui.theme.MediaPlayerImplTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MediaPlayerImplTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MediaPlayerImplTheme {
+        Greeting("Android")
+    }
+}
