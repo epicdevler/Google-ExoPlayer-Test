49411d07b5955f2c9f1c8e92814770631b44c8f6
Nwadike Philip <philiponwadike123@gmail.com>
2022-06-10 09:20:57 +0100

    Modified Player composable ui

--- app/src/main/java/com/epicdevler/practice/mediaplayerimpl/MainActivity.kt
+++ app/src/main/java/com/epicdevler/practice/mediaplayerimpl/MainActivity.kt
@@ -3,6 +3,7 @@ package com.epicdevler.practice.mediaplayerimpl
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -10,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.epicdevler.practice.mediaplayerimpl.ui.theme.MediaPlayerImplTheme
@@ -34,7 +36,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Player() {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Button(onClick = { startPlayer() }) {
             Text(text = "Play")
         }
